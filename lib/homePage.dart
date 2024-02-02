e5719dcd117e6d01e6977386af3ccdb6dabb34a5
ravenuy17 <154998021+ravenuy17@users.noreply.github.com>
2024-02-02 14:28:20 +0800

    Changes yet again

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:flutter_application_5_experiment/constant.dart';
-import 'package:flutter_application_5_experiment/util/numKey.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -30,8 +40,8 @@ class _HomePageState extends State<HomePage> {
   ];
 
   // Question variables
-  int numberA = 1;
-  int numberB = 1;
+  int numberA = Random().nextInt(1000);
+  int numberB = Random().nextInt(1000);
   String operator = '+';
 
   // User's answer
@@ -41,35 +51,37 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
-      body: Column(
-        children: [
-          // Level progress, get 5 to proceed to the next level
-          Container(
-            height: 100,
-            color: Colors.amberAccent,
-          ),
-          // Question
-          Expanded(
-            child: Container(
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            // Level progress, get 5 to proceed to the next level
+            Container(
+              height: 100,
+              color: Colors.amberAccent,
+            ),
+            // Question
+            Container(
+              height: MediaQuery.of(context).size.height * 0.3,
               child: Center(
                 child: Text(
                   '$numberA $operator $numberB = $userAnswer',
-                  style: whiteTextStyle,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
               color: Colors.blue,
             ),
-          ),
 
-          // Number pad
-          Expanded(
-            flex: 3,
-            child: Container(
+            // Number pad
+            Container(
+              height: MediaQuery.of(context).size.height * 0.7,
               child: Padding(
                 padding: const EdgeInsets.all(4.0),
                 child: GridView.builder(
                   itemCount: numberPad.length,
-                  physics: const NeverScrollableScrollPhysics(),
+                  physics: NeverScrollableScrollPhysics(),
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 4,
                   ),
@@ -82,8 +94,8 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -109,73 +121,86 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  // Check result function
   void checkResult() {
-    // Validate user's answer
-    if (userAnswer.isNotEmpty) {
-      try {
-        int userAnswerInt = int.parse(userAnswer);
-
-        int result = operator == '+'
-            ? numberA + numberB
-            : 0; // You can expand this for other operators
-
-        if (result == userAnswerInt) {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                backgroundColor: Colors.blueGrey,
-                content: Container(
-                  height: 250,
-                  color: Colors.blueGrey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Text('Great job!', style: whiteTextStyle),
-                      GestureDetector(
-                        onTap: goToNextQuestion,
-                        child: Container(
-                          padding: EdgeInsets.all(4),
-                          decoration: BoxDecoration(
-                            color: Colors.indigo,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Icon(
-                            Icons.arrow_forward,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
-          );
-        } else {
-          print('Try again!');
-        }
-      } catch (e) {
-        print('Invalid input. Please enter a valid number.');
-      }
+    int result = numberA + numberB;
+    if (int.tryParse(userAnswer) == result) {
+      showGreatJobModal();
     } else {
-      print('Answer cannot be empty. Please enter a number.');
+      showTryAgainModal();
     }
   }
 
-  // Go to next question function
-  void goToNextQuestion() {
-    // Dismiss alert
-    Navigator.of(context).pop();
+  void showGreatJobModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Great Job!'),
+          content: Text('You got it right!'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                generateNewQuestion();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
-    // Reset values
+  void showTryAgainModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Try Again'),
+          content: Text('Oops! Your answer is incorrect. Please try again.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void generateNewQuestion() {
     setState(() {
+      // Update question variables for a new question
+      numberA = Random().nextInt(1000);
+      numberB = Random().nextInt(1000);
       userAnswer = '';
     });
+  }
+}
 
-    // Create new question
-    numberA = Random().nextInt(1000);
-    numberB = Random().nextInt(1000);
+class MyButton extends StatelessWidget {
+  final String child;
+  final VoidCallback onTap;
+
+  MyButton({required this.child, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+        ),
+        child: Text(
+          child,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
