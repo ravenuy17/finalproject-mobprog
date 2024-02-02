d3a4427bb9bfc5a7c5818c919c3b32d43a1815af
ravenuy17 <154998021+ravenuy17@users.noreply.github.com>
2024-02-02 13:29:47 +0800

    Finish

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
 import 'dart:math';
 
-import 'package:flutter/material.dart';
+import 'package:flutter_application_5_experiment/constant.dart';
+import 'package:flutter_application_5_experiment/util/numKey.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,34 +13,86 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Number pad list
-  static const List<String> numberPad = [
-    '7',
-    '8',
-    '9',
-    'C',
-    '4',
-    '5',
-    '6',
-    'DEL',
-    '1',
-    '2',
-    '3',
-    '=',
-    '0'
+  List<String> numberPad = [
+    "7",
+    "8",
+    "9",
+    "C",
+    "4",
+    "5",
+    "6",
+    "DEL",
+    "1",
+    "2",
+    "3",
+    "=",
+    "0",
   ];
 
-  // NumA and NumB
+  // Question variables
   int numberA = 1;
   int numberB = 1;
+  String operator = '+';
 
-  // User answer
+  // User's answer
   String userAnswer = '';
 
-  // Button tapped
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Column(
+        children: [
+          // Level progress, get 5 to proceed to the next level
+          Container(
+            height: 100,
+            color: Colors.amberAccent,
+          ),
+          // Question
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Text(
+                  '$numberA $operator $numberB = $userAnswer',
+                  style: whiteTextStyle,
+                ),
+              ),
+              color: Colors.blue,
+            ),
+          ),
+
+          // Number pad
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: GridView.builder(
+                  itemCount: numberPad.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                  ),
+                  itemBuilder: (context, index) {
+                    return MyButton(
+                      child: numberPad[index],
+                      onTap: () => buttonTapped(numberPad[index]),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Button tapped function
   void buttonTapped(String button) {
     setState(() {
       if (button == '=') {
-        // Calculate the result
+        // Calculate result
         checkResult();
       } else if (button == 'C') {
         // Clear input
@@ -48,15 +102,20 @@ class _HomePageState extends State<HomePage> {
         if (userAnswer.isNotEmpty) {
           userAnswer = userAnswer.substring(0, userAnswer.length - 1);
         }
-      } else if (userAnswer.length < 5) {
+      } else {
+        // Append the button value to the user's answer
         userAnswer += button;
       }
     });
   }
 
-  // Check result
+  // Check result function
   void checkResult() {
-    if (numberA + numberB == int.parse(userAnswer)) {
+    int result = operator == '+'
+        ? numberA + numberB
+        : 0; // You can expand this for other operators
+
+    if (result == int.parse(userAnswer)) {
       showDialog(
         context: context,
         builder: (context) {
@@ -68,7 +127,7 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Text('Great job!', style: TextStyle(color: Colors.white)),
+                  Text('Great job!', style: whiteTextStyle),
                   GestureDetector(
                     onTap: goToNextQuestion,
                     child: Container(
@@ -94,7 +153,7 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  // Go to next question
+  // Go to next question function
   void goToNextQuestion() {
     // Dismiss alert
     Navigator.of(context).pop();
@@ -107,99 +166,5 @@ class _HomePageState extends State<HomePage> {
     // Create new question
     numberA = Random().nextInt(1000);
     numberB = Random().nextInt(1000);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amberAccent[100],
-      body: Column(
-        children: <Widget>[
-          // Level progress
-          Container(height: 100, color: Colors.amber),
-          // Questions
-          Expanded(
-            child: Container(
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Expanded(
-                      child: Text(
-                        '$numberA + $numberB =',
-                        style: TextStyle(fontSize: 20),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    // Answer box
-                    GestureDetector(
-                      onTap: goToNextQuestion,
-                      child: Container(
-                        padding: EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          color: Colors.indigo,
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Icon(
-                          Icons.arrow_forward,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          // Number pad
-          Expanded(
-            flex: 3,
-            child: Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: GridView.builder(
-                itemCount: numberPad.length,
-                physics: const NeverScrollableScrollPhysics(),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                ),
-                itemBuilder: (context, index) {
-                  return MyButton(
-                    child: numberPad[index],
-                    onTap: () => buttonTapped(numberPad[index]),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  final String child;
-  final VoidCallback onTap;
-
-  const MyButton({required this.child, required this.onTap});
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: const EdgeInsets.all(4),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Center(
-          child: Text(
-            child,
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
-    );
   }
 }
