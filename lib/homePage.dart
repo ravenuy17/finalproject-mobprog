8e19b9104ba4930daf468b4524350ee08b7f13e4
ravenuy17 <154998021+ravenuy17@users.noreply.github.com>
2024-02-02 14:35:01 +0800

    Colors on the button

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -23,20 +23,20 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // Number pad list
-  List<String> numberPad = [
-    "7",
-    "8",
-    "9",
-    "C",
-    "4",
-    "5",
-    "6",
-    "DEL",
-    "1",
-    "2",
-    "3",
-    "=",
-    "0",
+  List<MyButtonData> numberPad = [
+    MyButtonData(child: "7", color: Colors.orange),
+    MyButtonData(child: "8", color: Colors.orange),
+    MyButtonData(child: "9", color: Colors.orange),
+    MyButtonData(child: "C", color: Colors.red),
+    MyButtonData(child: "4", color: Colors.orange),
+    MyButtonData(child: "5", color: Colors.orange),
+    MyButtonData(child: "6", color: Colors.orange),
+    MyButtonData(child: "DEL", color: Colors.red),
+    MyButtonData(child: "1", color: Colors.orange),
+    MyButtonData(child: "2", color: Colors.orange),
+    MyButtonData(child: "3", color: Colors.orange),
+    MyButtonData(child: "=", color: Colors.blue),
+    MyButtonData(child: "0", color: Colors.orange),
   ];
 
   // Question variables
@@ -87,8 +87,8 @@ class _HomePageState extends State<HomePage> {
                   ),
                   itemBuilder: (context, index) {
                     return MyButton(
-                      child: numberPad[index],
-                      onTap: () => buttonTapped(numberPad[index]),
+                      data: numberPad[index],
+                      onTap: () => buttonTapped(numberPad[index].child),
                     );
                   },
                 ),
@@ -115,19 +115,65 @@ class _HomePageState extends State<HomePage> {
           userAnswer = userAnswer.substring(0, userAnswer.length - 1);
         }
       } else {
-        // Append the button value to the user's answer
-        userAnswer += button;
+        // Append the button value to the user's answer if it's a valid input
+        if (isValidInput(button)) {
+          userAnswer += button;
+        }
       }
     });
   }
 
+  bool isValidInput(String input) {
+    // Define the valid characters that can be used in the input
+    List<String> validCharacters = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9"
+    ];
+
+    // Check if the input is a valid character
+    return validCharacters.contains(input);
+  }
+
   void checkResult() {
-    int result = numberA + numberB;
-    if (int.tryParse(userAnswer) == result) {
-      showGreatJobModal();
+    if (userAnswer.isEmpty) {
+      // Show an error modal if the answer is empty
+      showEmptyAnswerModal();
     } else {
-      showTryAgainModal();
+      int result = numberA + numberB;
+      if (int.tryParse(userAnswer) == result) {
+        showGreatJobModal();
+      } else {
+        showTryAgainModal();
+      }
     }
+  }
+
+  void showEmptyAnswerModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text('Please enter a valid answer before checking.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void showGreatJobModal() {
@@ -182,10 +228,10 @@ class _HomePageState extends State<HomePage> {
 }
 
 class MyButton extends StatelessWidget {
-  final String child;
+  final MyButtonData data;
   final VoidCallback onTap;
 
-  MyButton({required this.child, required this.onTap});
+  MyButton({required this.data, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
@@ -195,12 +241,20 @@ class MyButton extends StatelessWidget {
         alignment: Alignment.center,
         decoration: BoxDecoration(
           border: Border.all(color: Colors.white),
+          color: data.color,
         ),
         child: Text(
-          child,
+          data.child,
           style: TextStyle(color: Colors.white),
         ),
       ),
     );
   }
+}
+
+class MyButtonData {
+  final String child;
+  final Color color;
+
+  MyButtonData({required this.child, required this.color});
 }
