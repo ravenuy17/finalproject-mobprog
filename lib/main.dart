e3bedcae18ec9ecafb437be1cdfde512ef03d2aa
ravenuy17 <154998021+ravenuy17@users.noreply.github.com>
2024-02-17 21:59:15 +0800

    Changes yet again

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_5_experiment/subtraction.dart';
 import 'addition.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AdditionEasy(),
+      home: Subtraction(),
     );
   }
 }
